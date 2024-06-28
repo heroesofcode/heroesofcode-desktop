01dda593861e97d42e0f33399e7b91e33f92c031
joaolfp <joaolucasfp2001@gmail.com>
2024-06-28 03:28:08 -0300

    Create file home

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,11 +1,12 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import screens.Home
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "heroesofcode-desktop",
     ) {
-        App()
+        Home()
     }
 }
---
+++ composeApp/src/desktopMain/kotlin/screens/Home.kt
@@ -0,0 +1,10 @@
+package screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Home() {
+
+    Text("Hola")
+}
